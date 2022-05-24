fc1f1a39ed697c598c534fbd482234f8c9863474
TheWhiteDragonsRage <Dragonsrage99@gmail.com>
2022-05-24 15:52:46 +0100

    backup

--- Backup-Push.sh
+++ Backup-Push.sh
@@ -6,4 +6,5 @@ git commit -m "backup"
 git push -u origin master
 
 #https://stackoverflow.com/questions/35942754/how-can-i-save-username-and-password-in-git
+#https://lazarofilm.gitbook.io/3d-printing/creating-a-github-backup-for-klipper
 
